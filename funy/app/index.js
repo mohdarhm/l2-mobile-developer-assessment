b64a485d3fbc80f4ad3659664a0fe95efd885b1b
Shah Mohd. Arhum <mohd.arhum28@gmail.com>
2024-03-14 11:51:40 +0530

    stuff

--- funy/app/index.js
+++ funy/app/index.js
@@ -1,6 +1,6 @@
 // App.js
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
+// import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import StartScreen from './screens/StartScreen';
 import GameScreen from './screens/GameScreen';
